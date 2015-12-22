ae08fa98de9ed4ec679b4bca5513365fb6067b41
andwxh <andwxh@gmail.com>
2015-12-22 11:12:02 +0800

    using byte instead of string

--- check.rb
+++ check.rb
@@ -1,39 +1,40 @@
 #!/usr/bin/ruby
 
 def read_file(file_name)
-	bit_count = 0
+	byte_count = 0
 	File.open(file_name, 'rb') do |file|
-		while (buffer = file.read(1)) do
-			yield bit_count, buffer.unpack('B*')[0]
-			bit_count += 1
+		while (buffer = file.read(4096)) do
+			buffer.bytes.each do |byte|
+				yield byte_count, byte
+				byte_count += 1
+			end
 		end
 	end
 end
 
 $last_byte_num = 0
 
-def proc_follow_bytes(bstr)
-	if bstr.start_with?("10")
+def proc_follow_bytes(byte)
+	if byte[7] == 1 && byte[6] == 0
 		$last_byte_num -= 1
 		return true
 	end
 	return false
 end
 
-def proc_head_byte(bstr)
-	if $last_byte_num > 0
-		return false
-	end
+def proc_head_byte(byte)
+	return false if $last_byte_num > 0 || byte[6] == 0
+
 	$last_byte_num = case
-			when bstr.start_with?('110')
+			when byte[5] == 0 # 110xxxxx
 				1
-			when bstr.start_with?('1110')
+			when byte[4] == 0 # 1110xxxx
 				2
-			when bstr.start_with?('11110')
+			when byte[3] == 0 # 11110xxx
 				3
-			when bstr.start_with?('111110')
+			when byte[2] == 0 # 111110xx
 				4
-			when bstr.start_with?('1111110')
+			when byte[1] == 0 # 1111110x
 				5
 			else return false
 			end
@@ -44,11 +45,11 @@ def proc_ascii_char
 	return $last_byte_num == 0
 end
 
-read_file(ARGV[0]) do |count, bstr|
+read_file(ARGV[0]) do |count, byte|
 	if $last_byte_num > 0
-		ret = proc_follow_bytes(bstr)
-	elsif bstr.start_with?("1")
-		ret = proc_head_byte(bstr)
+		ret = proc_follow_bytes(byte)
+	elsif byte[7] == 1
+		ret = proc_head_byte(byte)
 	else
 		ret = proc_ascii_char()
 	end
